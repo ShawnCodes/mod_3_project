9dd25a055af296e9b6fed9ab3cb0051698f17ce2
Daniel Brazel <dbrazel4@gmail.com>
2018-05-30 21:31:53 -0400

    Endscreen DOM manipulation done

--- end.js
+++ end.js
@@ -1,11 +1,19 @@
-const endScreen = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-  text: `Game Over You Scored: `
-  // preload: preload,
-  // create: create,
-  // update: update
-})
+// const endScreen = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+//   text: `Game Over You Scored: `
+//   // preload: preload,
+//   // create: create,
+//   // update: update
+// })
+const div = document.getElementById('overlay')
+const h2 = document.createElement('h2')
+h2.innerText = "Game Over"
+const para = document.createElement('p')
+para.innerText = `Your score is ${score}`
 
-endScreen.add.text(16, 16, '', {
-  fontSize: '32px',
-  fill: '#000'
-})
+div.append(h2)
+div.append(para)
+
+// endScreen.add.text(16, 16, '', {
+//   fontSize: '32px',
+//   fill: '#000'
+// })
